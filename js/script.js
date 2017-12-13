10918820fe5ab6d4d2ac12296984849a0cb2ad5f
BL MacBook <BLMac@Brendas-MacBook-Pro.local>
2017-12-13 00:15:43 -0800

    Added sticky navigation and smooth scrolling; fixed about section; added tools in skills section

--- js/script.js
+++ js/script.js
@@ -2,11 +2,29 @@ $(document).ready(function(){
 	$(".icon").click(function() {
 		if ($("#myTopNav").attr("class") === "topNav"){
 			$(".topNav").addClass("responsive");
-			
 		}else{
 			$(".topNav.responsive").removeClass("responsive");
 		}
 	});
 
+	//http://codetheory.in/change-active-state-links-sticky-navigation-scroll/
+	var sections = $('.sectionHome'), nav = $('nav'), nav_height = nav.outerHeight();
+
+	$(window).on('scroll', function () {
+  		var cur_pos = $(this).scrollTop();
+ 
+  		sections.each(function() {
+    		var top = $(this).offset().top - nav_height,
+        	bottom = top + $(this).outerHeight();
+        });
+    });
+
+	nav.find('a').on('click', function () {
+	  	var $el = $(this), id = $el.attr('href');
+	 
+	  	$('html, body').animate({
+	    	scrollTop: $(id).offset().top - nav_height
+	    }, 500);
+	});
 
 });
